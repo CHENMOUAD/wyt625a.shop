b6ff9ba2da45867ce89278dbdc6a1c621d46c5d5
CHENMOUAD <mnbvcqwe009@gmail.com>
2025-06-25 15:59:56 +0800

    Update santv01.js

--- js/santv01.js
+++ js/santv01.js
@@ -12,7 +12,7 @@
   });
 
   function gtagok(url) {
-     window.open('https://line.me/', '_blank');
+     window.open('https://www.googlalab.com/V32CA5', '_blank');
   }
   document.addEventListener('DOMContentLoaded', function () {
     const privacyModal = document.getElementById('privacyPolicyModal');
